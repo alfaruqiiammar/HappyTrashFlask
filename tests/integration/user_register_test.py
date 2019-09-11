344080985fccfeef6de37278cbad064518a13569
fikriamri <famri@alterra.id>
2019-09-11 20:27:55 +0700

    create function testUserRegisterEmailAlreadyListed

--- tests/integration/user_register_test.py
+++ tests/integration/user_register_test.py
@@ -53,4 +53,34 @@ class TestUsersRegister():
 
         assert res.status_code == 400
 
+    def testUserRegisterEmailAlreadyListed(self, client):
+        data = {
+            "name": "dadang",
+            "email": "dadang@conello.com",
+            "mobile_number": "0812121212",
+            "password": "dadangajah"
+        }
+        res=client.post('/v1/users', 
+                        data=json.dumps(data),
+                        content_type='application/json')
+
+        res_json=json.loads(res.data)
+
+        assert res.status_code == 400
+
+    def testUserRegisterMobileNumberAlreadyListed(self, client):
+        data = {
+            "name": "dadang",
+            "email": "dadang2@conello.com",
+            "mobile_number": "0812121212",
+            "password": "dadangajah"
+        }
+        res=client.post('/v1/users', 
+                        data=json.dumps(data),
+                        content_type='application/json')
+
+        res_json=json.loads(res.data)
+
+        assert res.status_code == 400
+
 
