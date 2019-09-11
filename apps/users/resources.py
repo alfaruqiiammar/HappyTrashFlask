295b89d005f722f6c1438d94093085f7f9045e6c
fikriamri <famri@alterra.id>
2019-09-11 17:50:05 +0700

    make function __init__ and options for UserResource class

--- apps/users/resources.py
+++ apps/users/resources.py
@@ -7,3 +7,19 @@ from flask_jwt_extended import jwt_required, get_jwt_claims
 
 bp_users = Blueprint('users', __name__)
 api = Api(bp_users)
+
+class UsersResource(Resource):
+    """Class for storing HTTP request method for users table"""
+
+    def __init__(self):
+        """Init function needed to indicate this is a class, but never used"""
+        pass
+
+    def options(self, id):
+        """Flask-CORS function to make Flask allowing our apps to support cross origin resource sharing (CORS)"""
+        return {'Status': 'OK'}, 200
+
+
+
+api.add_resource(UsersResource, '')
+
