693686484e7354d4f0cb7bad78554222d5511982
resita-alterra <resita@alterra.id>
2019-09-12 14:03:39 +0700

    all testing passed

--- tests/order_management_test.py
+++ tests/order_management_test.py
@@ -13,7 +13,6 @@ class TestOrderManagement():
       }
     res = client.post('/v1/orders', data = json.dumps(order), headers = {'Authorization' : "Bearer " + token},content_type = 'application/json' )
 
-    # res = client.post('/v1/orders', data = json.dumps(order),content_type = 'application/json' )
     res_json = json.loads(res.data)
     TestOrderManagement.temp_order_id = res_json['id']
     assert res.status_code == 200
@@ -24,23 +23,21 @@ class TestOrderManagement():
       "photo" : "args"
       }
     res = client.post('/v1/orders', data = json.dumps(order), headers = {'Authorization' : "Bearer " + token},content_type = 'application/json' )
-    # res = client.post('/v1/orders', data = json.dumps(order), content_type = 'application/json' )
 
     res_json = json.loads(res.data)
     assert res.status_code == 400
 
-  # def testOrderPostInvalidUser(self, client):
-    # token = createTokenAdmin()
-    # order = {
-      # "adress" : "args",
-      # "time" : "2018-03-29T13:34:00.000",
-      # "photo" : "args"
-      # }
-    # # # res = client.post('/v1/orders', data = json.dumps(order), headers = {'Authorization' : "Bearer " + token},content_type = 'application/json' )
-# 
-    # res_json = json.loads(res.data)
-    # TestOrderManagement.temp_order_id = res_json['id']
-    # assert res.status_code == 403
+  def testOrderPostInvalidUser(self, client):
+    token = createTokenAdmin()
+    order = {
+      "adress" : "args",
+      "time" : "2018-03-29T13:34:00.000",
+      "photo" : "args"
+      }
+    res = client.post('/v1/orders', data = json.dumps(order), headers = {'Authorization' : "Bearer " + token},content_type = 'application/json' )
+
+    res_json = json.loads(res.data)
+    assert res.status_code == 403
 
   def testOrderPutCancelled(self,client):
     token = createTokenUser()
@@ -49,7 +46,7 @@ class TestOrderManagement():
     }
 
     res = client.put('/v1/orders/{}'.format(TestOrderManagement.temp_order_id), data = json.dumps(order_status),headers = {'Authorization' : "Bearer " + token}, content_type = 'application/json')
-    # res = client.put('/v1/orders/{}'.format(TestOrderManagement.temp_order_id), data = json.dumps(order_status), content_type = 'application/json')
+
     assert res.status_code == 200
 
   def testOrderPutInvalidStatus(self,client):
@@ -59,32 +56,32 @@ class TestOrderManagement():
     }
 
     res = client.put('/v1/orders/{}'.format(TestOrderManagement.temp_order_id), data = json.dumps(order_status),headers = {'Authorization' : "Bearer " + token}, content_type = 'application/json')
-    # res = client.put('/v1/orders/{}'.format(TestOrderManagement.temp_order_id), data = json.dumps(order_status), content_type = 'application/json')
+    
     assert res.status_code == 400
   
-  # def testOrderPutCancelledByAdmin(self,client):
-    # token = createTokenAdmin()
-    # order_status = {
-      # "status" : "cancelled"
-    # }
-    # # res = client.put('/v1/orders/{}'.format(TestOrderManagement.temp_order_id), data = json.dumps(order_status),headers = {'Authorization' : "Bearer " + token}, content_type = 'application/json')
-    # assert res.status_code == 403
+  def testOrderPutCancelledByAdmin(self,client):
+    token = createTokenAdmin()
+    order_status = {
+      "status" : "cancelled"
+    }
+    res = client.put('/v1/orders/{}'.format(TestOrderManagement.temp_order_id), data = json.dumps(order_status),headers = {'Authorization' : "Bearer " + token}, content_type = 'application/json')
+    assert res.status_code == 403
   
-  # def testPutRejected(self,client):
-  #   token = createTokenAdmin()
-  #   order_status = {
-  #     "status" : "cancelled"
-  #   }
-  #   res = client.put('/v1/orders/{}'.format(TestOrderManagement.temp_order_id), data = json.dumps(order_status),headers = {'Authorization' : "Bearer " + token}, content_type = 'application/json')
-  #   assert res.status_code == 200
+  def testPutRejected(self,client):
+    token = createTokenAdmin()
+    order_status = {
+      "status" : "rejected"
+    }
+    res = client.put('/v1/orders/{}'.format(TestOrderManagement.temp_order_id), data = json.dumps(order_status),headers = {'Authorization' : "Bearer " + token}, content_type = 'application/json')
+    assert res.status_code == 200
   
-  # def testPutRejectedByUser(self,client):
-    # token = createTokenUser()
-    # order_status = {
-      # "status" : "cancelled"
-    # }
-    # # res = client.put('/v1/orders/{}'.format(TestOrderManagement.temp_order_id), data = json.dumps(order_status),headers = {'Authorization' : "Bearer " + token}, content_type = 'application/json')
-    # assert res.status_code == 403
+  def testPutRejectedByUser(self,client):
+    token = createTokenUser()
+    order_status = {
+      "status" : "rejected"
+    }
+    res = client.put('/v1/orders/{}'.format(TestOrderManagement.temp_order_id), data = json.dumps(order_status),headers = {'Authorization' : "Bearer " + token}, content_type = 'application/json')
+    assert res.status_code == 403
 
   def testOrderPutNotFound(self,client):
     token = createTokenUser()
@@ -93,7 +90,7 @@ class TestOrderManagement():
     }
     
     res = client.put('/v1/orders/123456787', data = json.dumps(order_status),headers = {'Authorization' : "Bearer " + token}, content_type = 'application/json')
-    # res = client.put('/v1/orders/12345678'.format(TestOrderManagement.temp_order_id), data = json.dumps(order_status), content_type = 'application/json')
+    
     assert res.status_code == 404
 
   def testOrderPutDone(self, client):
@@ -111,29 +108,29 @@ class TestOrderManagement():
     }
 
     res = client.put('/v1/orders/{}'.format(TestOrderManagement.temp_order_id), data = json.dumps(order_status), headers = {'Authorization' : "Bearer " + token},content_type = 'application/json')
-    # res = client.put('/v1/orders/{}'.format(TestOrderManagement.temp_order_id), data = json.dumps(order_status), content_type = 'application/json')
+    
     assert res.status_code == 200  
 
-  # def testOrderPutDoneByUser(self, client):
-    # token = createTokenUser()
-    # order_status = {
-      # "status" : "done",
-      # "details" : [
-		        # {"trash_id": 8,
-			      # "qty" : 2.9
-		        # },
-		        # {"trash_id": 9,
-			      # "qty" : 1.2
-	          # }
-			      # ]
-    # }
-# 
-    # # res = client.put('/v1/orders/{}'.format(TestOrderManagement.temp_order_id), data = json.dumps(order_status), headers = {'Authorization' : "Bearer " + token},content_type = 'application/json')
-    # assert res.status_code == 403    
+  def testOrderPutDoneByUser(self, client):
+    token = createTokenUser()
+    order_status = {
+      "status" : "done",
+      "details" : [
+		        {"trash_id": 8,
+			      "qty" : 2.9
+		        },
+		        {"trash_id": 9,
+			      "qty" : 1.2
+	          }
+			      ]
+    }
+
+    res = client.put('/v1/orders/{}'.format(TestOrderManagement.temp_order_id), data = json.dumps(order_status), headers = {'Authorization' : "Bearer " + token},content_type = 'application/json')
+    assert res.status_code == 403    
   
   def testOrderGet(self,client):
     token = createTokenAdmin()
-    # res = client.get('/v1/orders',content_type = 'application/json')
+
     res = client.get('/v1/orders',headers = {'Authorization' : "Bearer " + token},content_type = 'application/json')
     assert res.status_code == 200
 
@@ -145,14 +142,14 @@ class TestOrderManagement():
     res = client.options('/v1/orders/user')
     assert res.status_code == 200
   
-  # def testUserOrderGet(self, client):
-    # token = createTokenUser()
-    # # res = client.get('v1/orders/user', headers={'Authorization' : "Bearer " + token}, content_type='application/json')
-# 
-    # assert res.status_code == 200
-# 
-  # def testUserOrderGetInvalidToken(self, client):
-    # token = createTokenAdmin()
-    # # res = client.get('v1/orders/user', headers={'Authorization' : "Bearer " + token}, content_type='application/json')
-# 
-    # assert res.status_code == 403
+  def testUserOrderGet(self, client):
+    token = createTokenUser()
+    res = client.get('v1/orders/user', headers={'Authorization' : "Bearer " + token}, content_type='application/json')
+
+    assert res.status_code == 200
+
+  def testUserOrderGetInvalidToken(self, client):
+    token = createTokenAdmin()
+    res = client.get('v1/orders/user', headers={'Authorization' : "Bearer " + token}, content_type='application/json')
+
+    assert res.status_code == 403
