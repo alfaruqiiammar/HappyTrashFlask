54e3739f608d49bca73f9634fa1fb60db14cd5c9
resita-alterra <resita@alterra.id>
2019-09-14 15:00:03 +0700

    ubah status di reward managemen test post

--- apps/rewards/model.py
+++ apps/rewards/model.py
@@ -2,6 +2,7 @@ from apps import db
 from flask_restful import fields
 import re
 
+
 class Rewards(db.Model):
     """Class for storing information about rewards table
 
@@ -24,8 +25,9 @@ class Rewards(db.Model):
     photo = db.Column(db.String(1000), nullable=False)
     stock = db.Column(db.Integer, nullable=False)
     status = db.Column(db.Boolean, nullable=False)
-    date_created  = db.Column(db.DateTime,  default=db.func.current_timestamp())
-    date_modified = db.Column(db.DateTime,  default=db.func.current_timestamp(), onupdate=db.func.current_timestamp())
+    date_created = db.Column(db.DateTime,  default=db.func.current_timestamp())
+    date_modified = db.Column(db.DateTime,  default=db.func.current_timestamp(
+    ), onupdate=db.func.current_timestamp())
 
     response_fields = {
         'id': fields.Integer,
@@ -33,7 +35,9 @@ class Rewards(db.Model):
         'point_to_claim': fields.Integer,
         'photo': fields.String,
         'stock': fields.Integer,
-        'status': fields.Boolean
+        'status': fields.Boolean,
+        'date_created': fields.DateTime,
+        'date_modified': fields.DateTime
     }
 
     def __init__(self, name, point_to_claim, photo, stock, status):
