75b8a0e44f14aa06cbeffdacb194c43c8b413a09
fikriamri <famri@alterra.id>
2019-09-15 12:15:04 +0700

    still trying to use encrypted variable

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+export DB_USER="${DB_USER}"
+export DB_PASSWORD="${DB_PASSWORD}"
 sudo docker stop backend
 sudo docker rm backend
 sudo docker rmi alfaruqi26/happy-trash-backend:v1
-sudo docker run -d -p 5000:5000 --name backend alfaruqi26/happy-trash-backend:v1
+sudo docker run --env DB_USER --env DB_PASSWORD -d -p 5000:5000 --name backend alfaruqi26/happy-trash-backend:v1
