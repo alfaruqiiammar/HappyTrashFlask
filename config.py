f85c45d66e4b4219c972f77cc08da0817e35ce15
resita-alterra <resita@alterra.id>
2019-09-16 03:25:18 +0700

    coba pake lokal dulu sementara deploy

--- config.py
+++ config.py
@@ -3,14 +3,16 @@ import os
 
 load_dotenv()
 
-# db_user = os.environ.get('DB_USER')
-# db_password = os.environ.get('DB_PASSWORD')
+db_user = os.environ.get('DB_USER')
+db_password = os.environ.get('DB_PASSWORD')
+#
+# db_user = 'HappyTrash'
+# db_password = 'HappyTerus'
 
-db_user = 'HappyTrash'
-db_password = 'HappyTerus'
 
 class Config():
     pass
+
 
 class DevelopmentConfig(Config):
     """Class for storing information about development database
@@ -20,8 +22,10 @@ class DevelopmentConfig(Config):
         SQLALCHEMI_DATABASE_URI: a string that contain information about uri to access development database
     """
     DEBUG = True
-    # SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://{}:{}@127.0.0.1:3306/happy_trash'.format(db_user, db_password)
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://{}:{}@happytrash.chrpcgbaee9q.ap-southeast-1.rds.amazonaws.com:3306/happy_trash'.format(db_user, db_password)
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://{}:{}@127.0.0.1:3306/happy_trash'.format(
+        db_user, db_password)
+    # SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://{}:{}@happytrash.chrpcgbaee9q.ap-southeast-1.rds.amazonaws.com:3306/happy_trash'.format(db_user, db_password)
+
 
 class TestingConfig(Config):
     """Class for storing information about testing database
@@ -32,4 +36,5 @@ class TestingConfig(Config):
     """
     TESTING = True
     # SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://{}:{}@127.0.0.1:3306/happy_trash_testing'.format(db_user, db_password)
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://{}:{}@happytrash.chrpcgbaee9q.ap-southeast-1.rds.amazonaws.com:3306/happy_trash_testing'.format(db_user, db_password)
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://{}:{}@happytrash.chrpcgbaee9q.ap-southeast-1.rds.amazonaws.com:3306/happy_trash_testing'.format(
+        db_user, db_password)
