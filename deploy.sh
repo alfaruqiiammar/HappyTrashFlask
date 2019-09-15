db5cb6b1a40e9b612f20c9d9982c1e7f5a7b9e04
fikriamri <famri@alterra.id>
2019-09-15 12:48:02 +0700

    revert change so the backend still can running properly

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@
 sudo docker stop backend
 sudo docker rm backend
 sudo docker rmi alfaruqi26/happy-trash-backend:v1
-sudo docker run --env DB_USER --env DB_PASSWORD -d -p 5000:5000 --name backend alfaruqi26/happy-trash-backend:v1
+sudo docker run -d -p 5000:5000 --name backend alfaruqi26/happy-trash-backend:v1
