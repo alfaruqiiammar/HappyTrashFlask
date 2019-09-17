38381cc5d14b990690873b99d054757a90e8468c
resita-alterra <resita@alterra.id>
2019-09-17 19:12:53 +0700

    test: trash_management_test OK

--- tests/trash_management_test.py
+++ tests/trash_management_test.py
@@ -1,160 +1,222 @@
 import json
 from . import app, client, cache, createTokenAdmin, createTokenUser, resetDatabase
 
+
 class TestTrashManagement():
-  resetDatabase()
-  temp_category_id = None
-  temp_trash_id = None
+    resetDatabase()
+    temp_category_id = None
+    temp_trash_id = None
+##############################TRASH CATEGORIES#################
 
-  def testTrashCategoriesPost(self, client):
-    token = createTokenAdmin()
-    category = {
-      "category_name" : "dummy_name"
-    }
-    res = client.post('/v1/trash_category', data = json.dumps(category),headers = {'Authorization' : "Bearer " + token}, content_type = 'application/json' )
+    ##### Post #####
 
-    res_json = json.loads(res.data)
-    TestTrashManagement.temp_category_id = res_json['id']
-    assert res.status_code == 200
-  
-  def testTrashCategoriesGetByAdmin(self, client):
-    token = createTokenAdmin()
-    res = client.get('/v1/trash_category', headers = {'Authorization' : "Bearer " + token},content_type = 'application/json')
-    assert res.status_code == 200
+    def testTrashCategoriesPost(self, client):
+        """test posting a new trash category to table with valid data an header"""
 
-  def testTrashCategoriesGetByUser(self, client):
-    token = createTokenUser()
-    res = client.get('/v1/trash_category', headers = {'Authorization' : "Bearer " + token},content_type = 'application/json')
-    assert res.status_code == 200
+        token = createTokenAdmin()
+        category = {
+            "category_name": "dummy_name"
+        }
+        res = client.post('/v1/trash_category', data=json.dumps(category), headers={
+                          'Authorization': "Bearer " + token}, content_type='application/json')
 
-  def testTrashCategoriesPut(self, client):
-    token = createTokenAdmin()
-    new_name = {
-      "category_name" : "newdummy"
-    }
+        res_json = json.loads(res.data)
+        TestTrashManagement.temp_category_id = res_json['id']
+        assert res.status_code == 200
 
-    res = client.put('/v1/trash_category/{}'.format(TestTrashManagement.temp_category_id), data = json.dumps(new_name),headers = {'Authorization' : "Bearer " + token}, content_type = 'application/json')
-    assert res.status_code == 200
+    ##### Get #####
+    def testTrashCategoriesGetByAdmin(self, client):
+        """Test getting all data from trash category table using token from an admin account"""
 
-  def testTrashCategoriesPutInvalidAdmin(self, client):
-    """Only admin can post a new trash category"""
+        token = createTokenAdmin()
+        res = client.get('/v1/trash_category', headers={
+                         'Authorization': "Bearer " + token}, content_type='application/json')
+        assert res.status_code == 200
 
-    token = createTokenUser()
-    new_name = {
-      "category_name" : "newdummy"
-    }
+    def testTrashCategoriesGetByUser(self, client):
+        """Test getting all data from trash category table using token from an user account"""
 
-    res = client.put('/v1/trash_category/{}'.format(TestTrashManagement.temp_category_id), data = json.dumps(new_name),headers = {'Authorization' : "Bearer " + token}, content_type = 'application/json')
-    assert res.status_code == 403
-  
-  def testTrashCategoriesPutInvalidName(self, client):
-    """Name can not be null"""
+        token = createTokenUser()
+        res = client.get('/v1/trash_category', headers={
+                         'Authorization': "Bearer " + token}, content_type='application/json')
+        assert res.status_code == 200
 
-    token = createTokenAdmin()
-    new_name = {
-      "category_name" : None
-    }
+    #### Put ####
 
-    res = client.put('/v1/trash_category/{}'.format(TestTrashManagement.temp_category_id), data = json.dumps(new_name),headers = {'Authorization' : "Bearer " + token}, content_type = 'application/json')
-    assert res.status_code == 400
-  
-  def testTrashCategoriesPutInvalidId(self, client):
-    token = createTokenAdmin()
-    new_name = {
-      "category_name" : "dummyname"
-    }
-    res = client.put('/v1/trash_category/none', data = json.dumps(new_name),headers = {'Authorization' : "Bearer " + token}, content_type = 'application/json')
-    assert res.status_code == 404
+    def testTrashCategoriesPut(self, client):
+        """test put a record in tras category table with valid token and data"""
 
-#######
-  def testTrashPost(self, client):
-    token = createTokenAdmin()
-    trash = {
-      "trash_category_id" : TestTrashManagement.temp_category_id,
-      "trash_name" : "test",
-      "photo" : "test",
-      "price" : 100,
-      "point" : 1
-    }
-    res = client.post('/v1/trash', data = json.dumps(trash),headers = {'Authorization' : "Bearer " + token}, content_type = 'application/json' )
+        token = createTokenAdmin()
+        new_name = {
+            "category_name": "newdummy"
+        }
 
-    res_json = json.loads(res.data)
-    TestTrashManagement.temp_trash_id = res_json['id']
-    assert res.status_code == 200
-  
-  def testTrashGet(self, client):
-    token = createTokenAdmin()
-    res = client.get('/v1/trash',headers = {'Authorization' : "Bearer " + token}, content_type = 'application/json')
-    assert res.status_code == 200
-  
-  def testTrashPut(self, client):
-    token = createTokenAdmin()
-    new_details = {
-      "trash_category_id" : 1,
-      "trash_name" : "test",
-      "photo" : "test",
-      "price" : 100,
-      "point" : 1
-    }
+        res = client.put('/v1/trash_category/{}'.format(TestTrashManagement.temp_category_id), data=json.dumps(
+            new_name), headers={'Authorization': "Bearer " + token}, content_type='application/json')
+        assert res.status_code == 200
 
-    res = client.put('/v1/trash/{}'.format(TestTrashManagement.temp_trash_id), data = json.dumps(new_details),headers = {'Authorization' : "Bearer " + token}, content_type = 'application/json')
-    assert res.status_code == 200
-  
-  def testTrashPutByUser(self, client):
-    """"User is not permitted to change trash credentials"""
+    def testTrashCategoriesPutInvalidAdmin(self, client):
+        """test put a record in tras category table with invalid token
+        Token used is a non-admin token, hence will raise 403(forbidden) error
+        """
 
-    token = createTokenUser()
-    new_details = {
-      "trash_category_id" : 1,
-      "trash_name" : "test",
-      "photo" : "test",
-      "price" : 100,
-      "point" : 1
-    }
+        token = createTokenUser()
+        new_name = {
+            "category_name": "newdummy"
+        }
 
-    res = client.put('/v1/trash/{}'.format(TestTrashManagement.temp_trash_id), data = json.dumps(new_details),headers = {'Authorization' : "Bearer " + token}, content_type = 'application/json')
-    assert res.status_code == 403
-  
-  def testTrashPutInvalidId(self, client):
-    """Can not update unavailable trash data"""
+        res = client.put('/v1/trash_category/{}'.format(TestTrashManagement.temp_category_id), data=json.dumps(
+            new_name), headers={'Authorization': "Bearer " + token}, content_type='application/json')
+        assert res.status_code == 403
 
-    token = createTokenAdmin()
-    new_details = {
-      "trash_name" : "test",
-      "photo" : "test",
-      "price" : 100,
-      "point" : 1
-    }
-    res = client.put('/v1/trash/none', data = json.dumps(new_details), headers = {'Authorization' : "Bearer " + token},content_type = 'application/json')
-    assert res.status_code == 404
+    def testTrashCategoriesPutInvalidName(self, client):
+        """test put a record in trash category table with invalid data.
+        category_name is null, hence will raise a 400(bad request) error"""
 
-  def testTrashDelete(self, client):
-    
-    token = createTokenAdmin()
-    res = client.delete('/v1/trash/{}'.format(TestTrashManagement.temp_trash_id), headers = {'Authorization' : "Bearer " + token},content_type = 'application/json')
-    assert res.status_code == 200 
+        token = createTokenAdmin()
+        new_name = {
+            "category_name": None
+        }
 
-  def testTrashDeleteInvalidId(self, client):
-    """Can not delete unavailable trash data"""
-    token = createTokenAdmin()
-    res = client.delete('/v1/trash/{}'.format(TestTrashManagement.temp_trash_id), headers = {'Authorization' : "Bearer " + token},content_type = 'application/json')
-    assert res.status_code == 404
-#######
+        res = client.put('/v1/trash_category/{}'.format(TestTrashManagement.temp_category_id), data=json.dumps(
+            new_name), headers={'Authorization': "Bearer " + token}, content_type='application/json')
+        assert res.status_code == 400
 
-  def testTrashCategoriesDelete(self, client):
-    token = createTokenAdmin()
-    res = client.delete('/v1/trash_category/{}'.format(TestTrashManagement.temp_category_id), headers = {'Authorization' : "Bearer " + token},content_type = 'application/json')
-    assert res.status_code == 200 
+    def testTrashCategoriesPutInvalidId(self, client):
+        """test put a record in trash category table with invalid id.
+        'none' is an invalid id, hence will raise a 404(Not Found) error"""
+        token = createTokenAdmin()
+        new_name = {
+            "category_name": "dummyname"
+        }
+        res = client.put('/v1/trash_category/none', data=json.dumps(new_name), headers={
+                         'Authorization': "Bearer " + token}, content_type='application/json')
+        assert res.status_code == 404
 
-  def testTrashCategoriesDeleteInvalidId(self, client):
-    token = createTokenAdmin()
-    res = client.delete('/v1/trash_category/{}'.format(TestTrashManagement.temp_category_id), headers = {'Authorization' : "Bearer " + token},content_type = 'application/json')
-    assert res.status_code == 404   
-  
-  def testOptionsTrash(self, client):
-    res = client.options('/v1/trash')
-    assert res.status_code == 200
+    ###################################TRASH#####################
 
-  def testOptionsTrashCategories(self, client):
-    res = client.options('/v1/trash_category')
-    assert res.status_code == 200
+    #### Post ####
+
+    def testTrashPost(self, client):
+        """test posting a new trash to table with valid data and authorization header"""
+
+        token = createTokenAdmin()
+        trash = {
+            "trash_category_id": TestTrashManagement.temp_category_id,
+            "trash_name": "test",
+            "photo": "test",
+            "price": 100,
+            "point": 1
+        }
+        res = client.post('/v1/trash', data=json.dumps(trash), headers={
+                          'Authorization': "Bearer " + token}, content_type='application/json')
+
+        res_json = json.loads(res.data)
+        TestTrashManagement.temp_trash_id = res_json['id']
+        assert res.status_code == 200
+
+    #### Get ####
+
+    def testTrashGet(self, client):
+        """Test getting all data from trashes table using valid token"""
+
+        token = createTokenAdmin()
+        res = client.get(
+            '/v1/trash', headers={'Authorization': "Bearer " + token}, content_type='application/json')
+        assert res.status_code == 200
+
+    #### Put ####
+
+    def testTrashPut(self, client):
+        """test put a record in tras tableh with valid token and data"""
+        token = createTokenAdmin()
+        new_details = {
+            "trash_category_id": 1,
+            "trash_name": "test",
+            "photo": "test",
+            "price": 100,
+            "point": 1
+        }
+
+        res = client.put('/v1/trash/{}'.format(TestTrashManagement.temp_trash_id), data=json.dumps(
+            new_details), headers={'Authorization': "Bearer " + token}, content_type='application/json')
+        assert res.status_code == 200
+
+    def testTrashPutByUser(self, client):
+        """test put a record in trashes table with invalid token
+        Token used is a non-admin token, hence will raise 403(forbidden) error
+        """
+
+        token = createTokenUser()
+        new_details = {
+            "trash_category_id": 1,
+            "trash_name": "test",
+            "photo": "test",
+            "price": 100,
+            "point": 1
+        }
+
+        res = client.put('/v1/trash/{}'.format(TestTrashManagement.temp_trash_id), data=json.dumps(
+            new_details), headers={'Authorization': "Bearer " + token}, content_type='application/json')
+        assert res.status_code == 403
+
+    def testTrashPutInvalidId(self, client):
+        """test put a record in trashes table with invalid id.
+        'none' is an invalid id, hence will raise a 404(Not Found) error"""
+
+        token = createTokenAdmin()
+        new_details = {
+            "trash_name": "test",
+            "photo": "test",
+            "price": 100,
+            "point": 1
+        }
+        res = client.put('/v1/trash/none', data=json.dumps(new_details), headers={
+                         'Authorization': "Bearer " + token}, content_type='application/json')
+        assert res.status_code == 404
+
+    def testTrashDelete(self, client):
+        """Test delete a record in trashes table with valid token"""
+
+        token = createTokenAdmin()
+        res = client.delete('/v1/trash/{}'.format(TestTrashManagement.temp_trash_id),
+                            headers={'Authorization': "Bearer " + token}, content_type='application/json')
+        assert res.status_code == 200
+
+    def testTrashDeleteInvalidId(self, client):
+        """Test delete a record in trashes table
+        trash with id in this test has been deleted in previous test, hence will get a 404(Not Found) error"""
+        token = createTokenAdmin()
+        res = client.delete('/v1/trash/{}'.format(TestTrashManagement.temp_trash_id),
+                            headers={'Authorization': "Bearer " + token}, content_type='application/json')
+        assert res.status_code == 404
+
+################ TRASH CATEGORIES DELETE ############
+
+    def testTrashCategoriesDelete(self, client):
+        """Test delete a record in trash category table with valid token and id"""
+
+        token = createTokenAdmin()
+        res = client.delete('/v1/trash_category/{}'.format(TestTrashManagement.temp_category_id),
+                            headers={'Authorization': "Bearer " + token}, content_type='application/json')
+        assert res.status_code == 200
+
+    def testTrashCategoriesDeleteInvalidId(self, client):
+        """Test delete a record in trash categories table
+        trash with id in this test has been deleted in previous test, hence will get a 404(Not Found) error"""
+        token = createTokenAdmin()
+        res = client.delete('/v1/trash_category/{}'.format(TestTrashManagement.temp_category_id),
+                            headers={'Authorization': "Bearer " + token}, content_type='application/json')
+        assert res.status_code == 404
+
+    def testOptionsTrash(self, client):
+        """Test options function in resource"""
+
+        res = client.options('/v1/trash')
+        assert res.status_code == 200
+
+    def testOptionsTrashCategories(self, client):
+        """Test options function in resource"""
+
+        res = client.options('/v1/trash_category')
+        assert res.status_code == 200
