4a98e864ecc638123f52a0ededc76b5d85852d4f
resita-alterra <resita@alterra.id>
2019-09-17 13:05:03 +0700

    reward history: resource, all commented, all sorted

--- apps/reward_histories/model.py
+++ apps/reward_histories/model.py
@@ -4,34 +4,41 @@ import datetime
 
 
 class RewardHistories(db.Model):
-  """Class for storing information about reward that users have got
+    """Class for storing information about reward that users have got
 
-    Attributes:
-        __tablename__: a string of table name
-        reward_id: an integer of reward's id
-        reward_name :the alias of the reward
-        user_id : an integer of user's id
-        created_at: a datetime that indicates when the user got the reward
-        response_field: a dictionary that will be used to be a guide when extracting data from database's field
-    """
-  __tablename__ = "reward_histories"
+      Attributes:
+          __tablename__: a string of table name
+          reward_id: an integer of reward's id
+          reward_name :the alias of the reward
+          user_id : an integer of user's id
+          created_at: a datetime that indicates when the user got the reward
+          response_field: a dictionary that will be used to be a guide when extracting data from database's field
+      """
+    __tablename__ = "reward_histories"
 
-  id = db.Column(db.Integer, primary_key = True)
-  reward_id = db.Column(db.Integer, nullable = False)
-  reward_name = db.Column(db.String(100), nullable = False)
-  user_id = db.Column(db.Integer, nullable = False)
-  created_at = db.Column(db.DateTime, default=datetime.datetime.utcnow)
+    id = db.Column(db.Integer, primary_key=True)
+    reward_id = db.Column(db.Integer, nullable=False)
+    reward_name = db.Column(db.String(100), nullable=False)
+    user_id = db.Column(db.Integer, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.datetime.utcnow)
 
-  response_fields = {
+    response_fields = {
         'id': fields.Integer,
         'reward_id': fields.Integer,
-        'reward_name' : fields.String,
-        'user_id' : fields.Integer,
+        'reward_name': fields.String,
+        'user_id': fields.Integer,
         'created_at': fields.DateTime
     }
 
-  def __init__(self, data):
-    self.reward_id = data['reward_id']
-    self.reward_name = data['reward_name']
-    self.user_id = data['user_id']
-      
+    def __init__(self, data):
+        """Inits Reward historie record with the data inputted
+
+          Args:
+              reward_id: an integer of reward's id
+              reward_name: a string of reward's name
+              user_id: an integer of user's id
+        """
+
+        self.reward_id = data['reward_id']
+        self.reward_name = data['reward_name']
+        self.user_id = data['user_id']
