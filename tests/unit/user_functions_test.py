dd84283181c633010b664a65e9766b90abdb1cdd
resita-alterra <resita@alterra.id>
2019-09-21 00:01:05 +0700

    unit testing w.o mock

--- tests/unit/user_functions_test.py
+++ tests/unit/user_functions_test.py
@@ -4,8 +4,37 @@ from apps.users.model import Users
 
 
 class TestUserFunctions():
+    """Class for tests functions in user's model"""
+
     resetDatabase()
 
     def testUserEmailValid(self):
-        assert Users.isEmailAddressValid('happy@trash') == False
-        assert Users.isEmailAddressValid('happy@trash.com') == True
+        """Test wether isEmailAdressValid function working correctly"""
+
+        assert Users.isEmailAddressValid(self, 'happy@trash') == False
+        assert Users.isEmailAddressValid(self, 'happy@trash.com') == True
+
+    def testUserNumberValid(self):
+        """Test wether isMobileNumberValid function working correctly"""
+
+        assert Users.isMobileNumberValid(self, "876542372864") == False
+        assert Users.isMobileNumberValid(self, "0876542372864") == True
+        assert Users.isMobileNumberValid(self, "064") == False
+
+    def testIsEmailExist(self):
+        """Test wether isEmailExist function working correctly"""
+
+        assert Users.isEmailExist("admin@admin.com") == True
+        assert Users.isEmailExist("foo@bar.com") == False
+
+    def testIsNumberExist(self):
+        """Test wether isMobileNumberExist function working correctly"""
+
+        assert Users.isMobileNumberExist("08812121212") == False
+        assert Users.isMobileNumberExist("088111111111") == False
+        assert Users.isMobileNumberExist("081122112211") == True
+
+# class TestGoogleApi():
+  # """class for testing google maps api using mock up"""
+#
+  # def testGoogleMapApi()
